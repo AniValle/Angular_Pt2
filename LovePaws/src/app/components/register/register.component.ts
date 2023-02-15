c087c1073496e839075ac68d3af5fe6e71a20815
Ani Valle <anilisethvalle@gmail.com>
2023-02-15 18:10:18 +0100

    register ready

--- LovePaws/src/app/components/register/register.component.ts
+++ LovePaws/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
-
 import { Component} from '@angular/core';
 import { FormGroup, FormControl,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { ServerServiceService } from 'src/app/services/server-service.service';
 
 
 @Component({
@@ -11,58 +13,83 @@ import { FormGroup, FormControl,Validators} from '@angular/forms';
 export class RegisterComponent {
 
   datos!:string;
+  miform!:FormGroup;
+  message!:string;
 
-  // Validations of the registration form
-  miform = new FormGroup({
+  constructor (public router:Router, private myhttp: ServerServiceService) {
+    // Validations of the registration form
+    this.miform = new FormGroup({
 
-    username:new FormControl('',[
-      Validators.required,
-      Validators.minLength(6),
-      Validators.maxLength(15),
-      Validators.pattern('^[a-zA-Z1-9]+$')
-    ]),
-    pass: new FormControl('',[
-      Validators.required,
-      Validators.minLength(8),
-      Validators.maxLength(10),
-      Validators.pattern('^[a-zA-Z0-9]+$')
-    ]),
-    confirm_pass: new FormControl('',[
-      Validators.required
-      // se ha de validar con una directiva
-    ]),
-    name: new FormControl('',[
-      Validators.required,
-      Validators.pattern('^[a-zA-Z` ]+$')
-    ]),
-    lastname: new FormControl('',[
-      Validators.required,
-      Validators.pattern('^[a-zA-Z ]+$')
-    ]),
-    email:new FormControl('',[
-      Validators.required,
-      Validators.email
-      //Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-    ]),
-    tel: new FormControl('',[
-      Validators.required,
-      Validators.pattern('^[6|7][0-9]{8}$')
-    ])
-  });
+      username:new FormControl('',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(15),
+        Validators.pattern('^[a-zA-Z1-9]+$')
+      ]),
+      pass: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(10),
+        Validators.pattern('^[a-zA-Z0-9]+$')
+      ]),
+      confirm_pass: new FormControl('',[
+        Validators.required
+        // se ha de validar con una directiva
+      ]),
+      name: new FormControl('',[
+        Validators.required,
+        Validators.pattern('^[a-zA-Z` ]+$')
+      ]),
+      lastname: new FormControl('',[
+        Validators.required,
+        Validators.pattern('^[a-zA-Z ]+$')
+      ]),
+      email:new FormControl('',[
+        Validators.required,
+        Validators.email
+        //Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ]),
+      tel: new FormControl('',[
+        Validators.required,
+        Validators.pattern('^[6|7][0-9]{8}$')
+      ])
+    });
 
+  }
+
+  testRegister():void {
+
+    this.myhttp.registerUser(this.miform.value).subscribe(
+      (result: User) => {
+        if(result == null){
+          this.message = 'Ocurrió un error al intentar guardar los datos';
+        }else{
+          console.log('from testRegister', result);
+          this.router.navigate(['/login']);
+        }
+      },
+      (error) => {
+        console.log('error from register', error)
+      }
+    )
+    
+  }
 
   // ------------------------- Button ---------------------------//
   submit():void {
-    console.log(this.miform.value);
-    this.datos= `
-    username:     ${this.miform.value.username}
-    pass:         ${this.miform.value.pass}
-    confirm_pass: ${this.miform.value.confirm_pass}
-    name:         ${this.miform.value.name}
-    lastname:     ${this.miform.value.lastname}
-    email:        ${this.miform.value.email}
-    tel:          ${this.miform.value.tel}
-    `
+
+    this.testRegister();
+
+    // console.log(this.miform.value);
+    // this.datos= `
+    // username:     ${this.miform.value.username}
+    // pass:         ${this.miform.value.pass}
+    // confirm_pass: ${this.miform.value.confirm_pass}
+    // name:         ${this.miform.value.name}
+    // lastname:     ${this.miform.value.lastname}
+    // email:        ${this.miform.value.email}
+    // tel:          ${this.miform.value.tel}
+    // `
   }
 
 }
